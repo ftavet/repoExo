7886a7ceeccd928d37793a293b393b361c561f0c
Jack Okissi <jokissi@humanbooster.com>
2017-10-09 15:05:21 +0200

    Branch Jack

--- src/defaut/App.java
+++ src/defaut/App.java
@@ -2,7 +2,7 @@ package defaut;
 
 public class App {
     public static void main(String[] args) {
-        Etudiant e = new Etudiant("Guillaume", 45481648);
-        System.out.println("Hello world !");
+        Etudiant e = new Etudiant("Jack", 0147200001);
+        System.out.println("Hello tout le monde !");
     }
 }
