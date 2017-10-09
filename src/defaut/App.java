c82ce32c47579f174000c4fc6d3212ce22d7c519
Jack Okissi <jokissi@humanbooster.com>
2017-10-09 15:42:45 +0200

    New prof

--- src/defaut/App.java
+++ src/defaut/App.java
@@ -4,20 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class App {
-    public static void main(String[] args) {
-    	Prof p1 = new Prof("Jacqueline","Duverger","Français");
-    	Prof p2 = new Prof("Robert","Momet","Maths");
-    	Prof p3 = new Prof("Jean-Michel","Alvalle","Espagnol");
-    	List<Prof> profs = new ArrayList<>();
-    	profs.add(p1);
-    	profs.add(p2);
-    	profs.add(p3);
-        Etudiant e = new Etudiant("Jack", 0147200001);
-        System.out.println("Hello tout le monde je m'appelle "+e.getName() );
-        System.out.println("Voici mes professeurs: ");
-        for(Prof p:profs) {
-        	System.out.println(p);
-        }
-    
-    }
+	public static void main(String[] args) {
+		Prof p1 = new Prof("Jacqueline", "Duverger", "Français");
+		Prof p2 = new Prof("Robert", "Momet", "Maths");
+		Prof p3 = new Prof("Jean-Michel", "Alvalle", "Espagnol");
+		Prof p4 = new Prof("Jack", "Beauregard", "Economie");
+		List<Prof> profs = new ArrayList<>();
+		profs.add(p1);
+		profs.add(p2);
+		profs.add(p3);
+		profs.add(p4);
+		Etudiant e = new Etudiant("Jack", 0147200001);
+		System.out.println("Hello tout le monde je m'appelle " + e.getName());
+		System.out.println("Voici mes professeurs: ");
+		for (Prof p : profs) {
+			System.out.println(p);
+		}
+
+	}
 }
