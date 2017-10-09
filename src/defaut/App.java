27e656cc6264c9e190ae6a4ba6518be8cde1c76f
Jack Okissi <jokissi@humanbooster.com>
2017-10-09 16:11:08 +0200

    New commit Prof

--- src/defaut/App.java
+++ src/defaut/App.java
@@ -9,11 +9,13 @@ public class App {
 		Prof p2 = new Prof("Robert", "Momet", "Maths");
 		Prof p3 = new Prof("Jean-Michel", "Alvalle", "Espagnol");
 		Prof p4 = new Prof("Jack", "Beauregard", "Economie");
+		Prof p5 = new Prof("Jean-Jacques", "Luther-King", "Droit");
 		List<Prof> profs = new ArrayList<>();
 		profs.add(p1);
 		profs.add(p2);
 		profs.add(p3);
 		profs.add(p4);
+		profs.add(p5);
 		Etudiant e = new Etudiant("Jack", 0147200001);
 		System.out.println("Hello tout le monde je m'appelle " + e.getName());
 		System.out.println("Voici mes professeurs: ");
